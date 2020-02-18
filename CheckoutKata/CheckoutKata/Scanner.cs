1df3a0e6810f9a38d26936798197e5be95169719
Lauren Weston <lauren.weston@cantarus.com>
2020-02-18 18:26:26 +0000

    Refactor: Single Responsibility
    Scanner logic is separate from checkout logic

---
+++ CheckoutKata/CheckoutKata/Scanner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout
+{
+    class Scanner
+    {
+        private Dictionary<char, int> pricingRules;
+
+        public Scanner(Dictionary<char, int> _pricingRules)
+        {
+            pricingRules = _pricingRules;
+        }
+    }
+}
